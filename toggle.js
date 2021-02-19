9438f4d67a6ffda4da62310554e8fb8f8d9fc7fb
chuabingya <74776084+chuabingya@users.noreply.github.com>
2021-02-19 10:49:54 +0800

    Create toggle.js

---
+++ toggle.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+
+export class Toggle extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+          tabType: "general"
+        }
+    }
+    toggleMe=(value)=>{
+      this.setState({
+        tabType:value
+      })
+    }
+    render() {
+        return (
+            <div>
+              <div className="mt-3">
+                <button className="btn btn-primary m-2" onClick={()=>this.toggleMe('Mountain')}>Mountain</button>
+                <button className="btn btn-secondary m-2" onClick={()=>this.toggleMe('Beaches')}>Beaches</button>
+                <button className="btn btn-warning m-2" onClick={()=>this.toggleMe('Birds')}>Birds</button>
+                <button className="btn btn-info m-2" onClick={()=>this.toggleMe('Food')} >Food</button>
+            </div>
+            <div>
+              {
+                this.state.tabType==='Mountain' && <div> Mountain </div>
+              }
+              {
+                this.state.tabType==='Beaches' && <div> Mountain </div>
+              }
+              {
+                this.state.tabType==='Beaches'  && <div> Mountain </div>
+              }
+              {
+                this.state.tabType==='Food' && <div> Mountain </div>
+              }
+              </div>
+            </div>
+              
+        )
+            }
+          }
